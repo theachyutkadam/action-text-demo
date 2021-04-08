0727bd74eef30e05128981f63096e565fead9b96
achyut kadam <theachyutkadam@gmail.com>
2021-04-08 11:45:02 +0530

    add precomplise false

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module ActionTextDemo
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
+    config.assets.initialize_on_precompile = false
+
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
